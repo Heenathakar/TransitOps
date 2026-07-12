9d943f24fcf0c07d670a11609fd48e3c581805c2
Heenathakar <hinathakar05@gmail.com>
2026-07-12 14:10:15 +0530

    feat(fleet): integrate Fleet Management module (Vehicles, Drivers, Maintenance)

--- fleet/admin.py
+++ fleet/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Vehicle,Driver,Maintenance
+
+
+
+admin.site.register(Vehicle)
+
+admin.site.register(Driver)
+
+admin.site.register(Maintenance)
